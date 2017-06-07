fa55e2c0c659e896015ac150cbebf9023e6487d6
James Fowkes <jamesfowkes@gmail.com>
2017-06-07 07:18:23 +0100

    Add logging, correct movement sizes

--- cryptology-barnsley-iris.ino
+++ cryptology-barnsley-iris.ino
@@ -8,21 +8,22 @@
 static const int SERVO_PIN = 2;
 static const int MAGLOCK_PIN = 9;
 static const int MOTOR_PIN_1 = 3;
-static const int MOTOR_PIN_2 = 4;
-static const int MOTOR_PIN_3 = 5;
+static const int MOTOR_PIN_2 = 5;
+static const int MOTOR_PIN_3 = 4;
 static const int MOTOR_PIN_4 = 6;
 static const int HOME_PIN = A0;
 static const int TRIGGER_PIN = A1;
 
 static const int SERVO_INITIAL_POSITION = 0;
-static const int SERVO_OPEN_POSITION = 180;
+static const int SERVO_OPEN_POSITION = 160;
 
-static const int IRIS_SPEED = 300;
-static const int IRIS_CLOSE_POSITION = 4096;
+static const int IRIS_SPEED = 10;
+static const int STEPS_PER_REV = 4096;
+static const int IRIS_CLOSE_POSITION = 2900;
 
 /* Local Objects/Variables */
 static Servo s_servo;
-static Stepper s_stepper(4096, MOTOR_PIN_1, MOTOR_PIN_2, MOTOR_PIN_3, MOTOR_PIN_4);
+static Stepper s_stepper(STEPS_PER_REV, MOTOR_PIN_1, MOTOR_PIN_2, MOTOR_PIN_3, MOTOR_PIN_4);
 
 /* Local Functions */
 
@@ -48,6 +49,7 @@ static void close_iris()
 
 void setup()
 {
+	Serial.begin(115200);
 	pinMode(SERVO_PIN, OUTPUT);
 	pinMode(MAGLOCK_PIN, OUTPUT);
 	pinMode(MOTOR_PIN_1, OUTPUT);
@@ -58,19 +60,34 @@ void setup()
 	pinMode(TRIGGER_PIN, INPUT_PULLUP);
 
 	s_servo.attach(SERVO_PIN);
+
+	Serial.println("Moving servo to initial position");
+
 	s_servo.write(SERVO_INITIAL_POSITION);
 
+	Serial.println("Finding home position");
+
 	open_iris();
+
+	Serial.println("Closing iris");
+
 	close_iris();
 
+	Serial.println("Waiting for trigger");
+
 	while(digitalRead(TRIGGER_PIN) != LOW) {}
+
+	Serial.println("Opening servo");
 
 	s_servo.write(SERVO_OPEN_POSITION);
 
 	delay(100);
 
+	Serial.println("Opening iris");
+
 	open_iris();
 
+	Serial.println("Iris open");
 }
 
 void loop()
